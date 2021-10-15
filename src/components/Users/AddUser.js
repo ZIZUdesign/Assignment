371f98a4bde1a8219f5390da92ad28d01be83f18
Zed <kahsayzeray@gmail.com>
2021-10-15 12:22:17 +0200

    notes added in AddUser comp

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,7 +1,7 @@
 
 
 import Card from '../UI/Card';
-import React, { useState } from'react'; 
+import React, { useState } from 'react'; 
 
 import classes from './AddUser.module.css'; 
 import Button from '../UI/Button';
@@ -17,14 +17,14 @@ const AddUser = (props) => {
 
    const [enteredUsername, setEnteredUsername] = useState('');
    const [enteredAge, setEnteredAge]= useState(''); 
-   const [error, setError]= useState(); 
+   const [error, setError] = useState(); 
 
 
     const addUserHandler = (event) => {
         event.preventDefault();
         // const enteredName = nameInputRef.current.value;
         // const enteredUserAge = ageInputRef.current.value; 
-        if(enteredUsername.trim().length ===0 || enteredAge.trim().length ===0){
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length ===0){
             setError({
                 title: 'Invalid input',
                 message: 'Please enter a valid name and age (non-empty values).'
@@ -85,13 +85,74 @@ const AddUser = (props) => {
         onChange={ageChangeHandler}
         // ref={ageInputRef}
         />
-        <Button type="submit">Add User </Button>
+        <Button type="submit"> Add user </Button>
      </form>
-     
-    </Card>
+      </Card>
 </div>
     
     );
+
+    
+// How TO USE useRef() TO READ THE CURRENT VALUE OF THE USER INPUT 
+// --------------------------------------------------------------------------
+// // we can also get access to the value of the input fields using useRef() react hook
+
+// const nameInputRef = useRef();
+// const ageInputRef = useRef();
+
+// // and in the input fields 
+// <input id= "username" type= "text" ref= {nameInputRef} />
+// <input id="age" type="number" ref= {ageInputRef} /> 
+// useRef has a 'current' property and, using this current propery we access the 
+// value of the user input 
+
+// when the form is submitted 
+// const addUserHandler = (event) => {
+//     event.preventDefault();
+
+//     const enteredName = nameInputRef.current.value();
+//     const enteredUserAge = ageInputRef.current.value();
+
+//     // check if the inputs are valid 
+//     if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0){
+//         setError({
+//             title: 'Invlaid input',
+//             message: 'Please enter a valid name and age (non-empty values.)',
+//         });
+//         return;
+//     }
+//     if(+enteredUserAge < 1){
+//         setError({
+//             title: 'Invalid age',
+//             message: 'Please enter a valid age (>0)',
+//         });
+
+//         return; 
+//     }
+
+//     props.onAddUser(enteredName, enteredUserAge);
+//     // Thought it is not recommended to manipulate the DOM using Refs, but it can 
+//     // be resett the values of the input fields as following
+//     nameInputRef.current.value = '';
+//     ageInputRef.current.value=''; 
+
+//           useRef vs useState
+//           ----------------------------
+// if your target is to read the value of the user input, but not to change it , 
+// better to use useRef() react hook.
+// if you want to get acces and at the same time to change the value of the input fields, better
+// to use useState() react hook
+//       controlled and un-controlled controls 
+//       -------------------------------------------
+// input fields which we have access using useRef are un-controlled. since we can't change
+// the values of the fields. 
+// input filds which have access to using useState are controlled since we can change the 
+// values of the input fields. 
+
+// }
+
 };
 
 export default AddUser;
+
+
